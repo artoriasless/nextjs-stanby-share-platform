261773a98b439d20a48edab9597a476adc35f807
Stan <tangjianyi1993@live.com>
2019-09-09 17:08:07 +0800

    fix: favico.ico not found fix the error that the url of favico.ico is 404 in development and production runtime environment

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,19 @@
 import Head from 'next/head';
 
+import config from 'config';
+
 import 'style/app.scss';
 
 const Layout = function(props) {
     const { children } = props;
+    const favUrl = `${config.dev ? '' : config.ossPublic.assetPrefix}/static/favicon.ico`;
 
     return (
         <>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <link rel="icon" type="image/x-icon" href={ favUrl }/>
+                <link rel="shortcut icon" type="image/x-icon" href={ favUrl }/>
             </Head>
             <div id="root">
                 { children }
