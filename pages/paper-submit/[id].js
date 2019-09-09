b0ec898d52419a61eb338ffb99fd5027b9765a84
Stan <tangjianyi1993@live.com>
2019-09-09 17:09:47 +0800

    fix: the link targeting "paper-submit" not work fix the issue that the router not changed when user click the href link targeting to "paper-submit", due to the usage of preload and dynamic import

--- pages/paper-submit/[id].js
+++ pages/paper-submit/[id].js
@@ -1,97 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { withRouter } from 'next/router';
 
+import config from 'config';
 import {
     Layout,
 } from 'components';
 
-import 'jodit/build/jodit.min.css';
-
 const Editor = function(props) {
     // https://xdsoft.net/jodit/
-    dynamic(() => import('jodit'), {
-        ssr: false
-    });
-
     const JoditEditor = dynamic(() => import('jodit-react'), {
         ssr: false
     });
-    const paperContent = 'title';
-    const changeHandler = val => {
-        console.info('change', val);
-    };
-    const editorConfig = {
-        readonly: false,
-        buttons: [
-            'font', 'fontsize', 'brush', 'paragraph', 'align',
-            '|',
-            'bold', 'strikethrough', 'underline', 'italic',
-            '|',
-            'superscript', 'subscript',
-            '|',
-            'ul', 'ol',
-            '|',
-            'outdent', 'indent',
-            '|',
-            'undo', 'redo',
-            '\n',
-            'hr',
-            'eraser',
-            'copyformat',
-            '|',
-            'symbol',
-            'fullsize',
-            '|',
-            'image', 'file', 'video', 'table', 'link',
-            '|',
-        ],
-        extraButtons: [
-            {
-                name: 'upload',
-                icon: 'upload',
-                exec: function (editor) {
-                    console.info('upload');
-                }
-            },
-            {
-                name: 'save',
-                icon: 'save',
-                exec: function (editor) {
-                    console.info('save');
-                }
-            },
-        ]
-    };
-    const $editorRef = typeof window === 'undefined' ? null : window.jodit;
 
-    return (
-        <JoditEditor
-            editorRef={ $editorRef }
-            value={ paperContent }
-            config={ editorConfig }
-            onChange={ changeHandler }
-        />
-    );
+    if (typeof window === 'undefined') {
+        return null;
+    } else {
+        const paperContent = 'title';
+        const changeHandler = val => {
+            console.info('change', val);
+        };
+        const editorConfig = {
+            readonly: false,
+            buttons: [
+                'font', 'fontsize', 'brush', 'paragraph', 'align',
+                '|',
+                'bold', 'strikethrough', 'underline', 'italic',
+                '|',
+                'superscript', 'subscript',
+                '|',
+                'ul', 'ol',
+                '|',
+                'outdent', 'indent',
+                '|',
+                'undo', 'redo',
+                '\n',
+                'hr',
+                'eraser',
+                'copyformat',
+                '|',
+                'symbol',
+                'fullsize',
+                '|',
+                'image', 'file', 'video', 'table', 'link',
+                '|',
+            ],
+            extraButtons: [
+                {
+                    name: 'upload',
+                    icon: 'upload',
+                    exec: function (editor) {
+                        console.info('upload');
+                    }
+                },
+                {
+                    name: 'save',
+                    icon: 'save',
+                    exec: function (editor) {
+                        console.info('save');
+                    }
+                },
+            ]
+        };
+        const $editorRef = typeof window === 'undefined' ? null : window.jodit;
+
+        return (
+            <JoditEditor
+                editorRef={ $editorRef }
+                value={ paperContent }
+                config={ editorConfig }
+                onChange={ changeHandler }
+            />
+        );
+    }
 };
 const PaperEdit = withRouter(function(props) {
+    const [intervalVal, setIntervalVal] = useState(0);
+    const assetPrefix = config.dev ? '' : config.ossPublic.assetPrefix;
     const { query } = props.router || {};
     const {
         id = ''
     } = query || {};
 
+    useEffect(() => {
+        const intervalTask = setInterval(() => {
+            if (typeof window !== 'undefined' && window.jodit) {
+                clearInterval(intervalTask);
+            }
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(intervalTask);
+        }, 10000);
+    }, [intervalVal]);
+
     return (
         <>
             <Head>
                 <title>Paper Edit Page { id }</title>
+                <link rel="stylesheet" href={ `${assetPrefix}/static/plugins/jodit/index.css` }/>
+                <script src={ `${assetPrefix}/static/plugins/jodit/index.js` }></script>
             </Head>
             <Layout>
                 <h1>Paper Edit Page</h1>
                 <div>
                     <h2>paper id : { id }</h2>
                     <div style={{ margin: '20px 100px' }}>
-                        <Editor/>
+                        <Editor interval={ intervalVal }/>
                     </div>
                 </div>
             </Layout>
